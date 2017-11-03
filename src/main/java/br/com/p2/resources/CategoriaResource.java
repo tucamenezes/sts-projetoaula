92e37fd886ab17d408369d3b5d805a7588be6571
Arthur Menezes <arthur.menezes@criativasolucoes.com>
2017-11-03 00:42:55 -0400

    criando classe de dominio

--- src/main/java/br/com/p2/resources/CategoriaResource.java
+++ src/main/java/br/com/p2/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package br.com.p2.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.p2.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,16 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está ok!!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 
 }
